978a9478461d8b756b2968f0624110d369b0fc4c
Artem Ryzhikov <artemryzhikov@gmail.com>
2020-12-24 20:02:52 +0300

    KL CPD implemented. Not tested!

--- model.py
+++ model.py
@@ -20,6 +20,8 @@ from sklearn.metrics.pairwise import euclidean_distances
 
 from data_loader import DataLoader
 from optim import Optim
+from types import SimpleNamespace
+from tqdm import trange
 
 
 def median_heuristic(X, beta=0.5):
@@ -29,53 +31,14 @@ def median_heuristic(X, beta=0.5):
     beta_list = [beta**2, beta**1, 1, (1.0/beta)**1, (1.0/beta)**2]
     return [med_sqdist * b for b in beta_list]
 
-
-# X_p_enc: batch_size x seq_len x hid_dim
-# X_f_enc: batch_size x seq_len x hid_dim
-# hid_dim could be either dataspace_dim or codespace_dim
-# return: MMD2(X_p_enc[i,:,:], X_f_enc[i,:,:]) for i = 1:batch_size
-def batch_mmd2_loss(X_p_enc, X_f_enc, sigma_var):
-    # some constants
-    n_basis = 1024
-    gumbel_lmd = 1e+6
-    cnst = math.sqrt(1. / n_basis)
-    n_mixtures = sigma_var.size(0)
-    n_samples = n_basis * n_mixtures
-    batch_size, seq_len, nz = X_p_enc.size()
-
-    # gumbel trick to get masking matrix to uniformly sample sigma
-    # input: (batch_size*n_samples, nz)
-    # output: (batch_size, n_samples, nz)
-    def sample_gmm(W, batch_size):
-        U = torch.cuda.FloatTensor(batch_size*n_samples, n_mixtures).uniform_()
-        sigma_samples = F.softmax(U * gumbel_lmd).matmul(sigma_var)
-        W_gmm = W.mul(1. / sigma_samples.unsqueeze(1))
-        W_gmm = W_gmm.view(batch_size, n_samples, nz)
-        return W_gmm
-
-    W = Variable(torch.cuda.FloatTensor(batch_size*n_samples, nz).normal_(0, 1))
-    W_gmm = sample_gmm(W, batch_size)                                   # batch_size x n_samples x nz
-    W_gmm = torch.transpose(W_gmm, 1, 2).contiguous()                   # batch_size x nz x n_samples
-    XW_p = torch.bmm(X_p_enc, W_gmm)                                    # batch_size x seq_len x n_samples
-    XW_f = torch.bmm(X_f_enc, W_gmm)                                    # batch_size x seq_len x n_samples
-    z_XW_p = cnst * torch.cat((torch.cos(XW_p), torch.sin(XW_p)), 2)
-    z_XW_f = cnst * torch.cat((torch.cos(XW_f), torch.sin(XW_f)), 2)
-    batch_mmd2_rff = torch.sum((z_XW_p.mean(1) - z_XW_f.mean(1))**2, 1)
-    return batch_mmd2_rff
-
-
-
-
 class NetG(nn.Module):
-    def __init__(self, args, data):
-        super(NetG, self).__init__()
-        self.wnd_dim = args.wnd_dim
-        self.var_dim = data.var_dim
-        self.D = data.D
-        self.RNN_hid_dim = args.RNN_hid_dim
+    def __init__(self, var_dim, RNN_hid_dim:int=10, num_layers:int=1):
+        super().__init__()
+        self.var_dim = var_dim
+        self.RNN_hid_dim = RNN_hid_dim
 
-        self.rnn_enc_layer = nn.GRU(self.var_dim, self.RNN_hid_dim, num_layers=1, batch_first=True)
-        self.rnn_dec_layer = nn.GRU(self.var_dim, self.RNN_hid_dim, num_layers=1, batch_first=True)
+        self.rnn_enc_layer = nn.GRU(self.var_dim, self.RNN_hid_dim, num_layers=num_layers, batch_first=True)
+        self.rnn_dec_layer = nn.GRU(self.var_dim, self.RNN_hid_dim, num_layers=num_layers, batch_first=True)
         self.fc_layer = nn.Linear(self.RNN_hid_dim, self.var_dim)
 
     # X_p:   batch_size x wnd_dim x var_dim (Encoder input)
@@ -98,16 +61,14 @@ class NetG(nn.Module):
 
 
 class NetD(nn.Module):
-    def __init__(self, args, data):
+    def __init__(self, var_dim, RNN_hid_dim:int=10, num_layers:int=1):
         super(NetD, self).__init__()
 
-        self.wnd_dim = args.wnd_dim
-        self.var_dim = data.var_dim
-        self.D = data.D
-        self.RNN_hid_dim = args.RNN_hid_dim
+        self.var_dim = var_dim
+        self.RNN_hid_dim = RNN_hid_dim
 
-        self.rnn_enc_layer = nn.GRU(self.var_dim, self.RNN_hid_dim, batch_first=True)
-        self.rnn_dec_layer = nn.GRU(self.RNN_hid_dim, self.var_dim, batch_first=True)
+        self.rnn_enc_layer = nn.GRU(self.var_dim, self.RNN_hid_dim, num_layers=num_layers, batch_first=True)
+        self.rnn_dec_layer = nn.GRU(self.RNN_hid_dim, self.var_dim, num_layers=num_layers, batch_first=True)
 
     def forward(self, X):
         X_enc, _ = self.rnn_enc_layer(X)
@@ -115,170 +76,95 @@ class NetD(nn.Module):
         return X_enc, X_dec
 
 
-# Y, L should be numpy array
-def valid_epoch(loader, data, netD, batch_size, Y_true, L_true):
-    netD.eval()
-    Y_pred = []
-    for inputs in loader.get_batches(data, batch_size, shuffle=False):
-        X_p, X_f = inputs[0], inputs[1]
+class KL_CPD(nn.Module):
+    def __init__(self, critic_iters:int, lambda_ae:float=0.001, lambda_real:float=0.1):
+        super().__init__()
+        self.netD = NetD(var_dim)
+        self.netG = NetG(var_dim)
+        self.critic_iters = critic_iters
+
+        # must be defined in fit() method
+        optG = torch.optim.AdamW(self.netG.parameters(), lr=lr,
+                   weight_decay=weight_decay,
+                   momentum=momentum)
+
+        optD = torch.optim.AdamW(self.netD.parameters(), lr=lr,
+                   weight_decay=weight_decay,
+                   momentum=momentum)
+
+        torch.nn.utils.clip_grad_norm_(self.netG.parameters(), grad_clip)
+
+
+
+    @property
+    def device(self):
+        return next(self.parameters()).device
+
+    def __mmd2_loss(self, X_p_enc, X_f_enc):
+        sigma_var = self.sigma_var
+
+        # some constants
+        n_basis = 1024
+        gumbel_lmd = 1e+6
+        cnst = math.sqrt(1. / n_basis)
+        n_mixtures = sigma_var.size(0)
+        n_samples = n_basis * n_mixtures
+        batch_size, seq_len, nz = X_p_enc.size()
+
+        # gumbel trick to get masking matrix to uniformly sample sigma
+        # input: (batch_size*n_samples, nz)
+        # output: (batch_size, n_samples, nz)
+        def sample_gmm(W, batch_size):
+            U = torch.FloatTensor(batch_size*n_samples, n_mixtures).uniform_().to(self.device)
+            sigma_samples = F.softmax(U * gumbel_lmd).matmul(sigma_var)
+            W_gmm = W.mul(1. / sigma_samples.unsqueeze(1))
+            W_gmm = W_gmm.view(batch_size, n_samples, nz)
+            return W_gmm
+
+        W = Variable(torch.FloatTensor(batch_size*n_samples, nz).normal_(0, 1).to(self.device))
+        W_gmm = sample_gmm(W, batch_size)                                   # batch_size x n_samples x nz
+        W_gmm = torch.transpose(W_gmm, 1, 2).contiguous()                   # batch_size x nz x n_samples
+        XW_p = torch.bmm(X_p_enc, W_gmm)                                    # batch_size x seq_len x n_samples
+        XW_f = torch.bmm(X_f_enc, W_gmm)                                    # batch_size x seq_len x n_samples
+        z_XW_p = cnst * torch.cat((torch.cos(XW_p), torch.sin(XW_p)), 2)
+        z_XW_f = cnst * torch.cat((torch.cos(XW_f), torch.sin(XW_f)), 2)
+        batch_mmd2_rff = torch.sum((z_XW_p.mean(1) - z_XW_f.mean(1))**2, 1)
+        return batch_mmd2_rff
+
+    def forward(self, X_p:torch.Tensor, X_f:torch.Tensor):
         batch_size = X_p.size(0)
 
-        X_p_enc, _ = netD(X_p)
-        X_f_enc, _ = netD(X_f)
-        Y_pred_batch = batch_mmd2_loss(X_p_enc, X_f_enc, sigma_var)
-        Y_pred.append(Y_pred_batch.data.cpu().numpy())
-    Y_pred = np.concatenate(Y_pred, axis=0)
+        X_p_enc, _ = self.netD(X_p)
+        X_f_enc, _ = sself.netD(X_f)
+        Y_pred_batch = self.batch_mmd2_loss(X_p_enc, X_f_enc, sigma_var)
 
-    L_pred = Y_pred
-    fp_list, tp_list, thresholds = sklearn.metrics.roc_curve(L_true, L_pred)
-    auc = sklearn.metrics.auc(fp_list, tp_list)
-    eval_dict = {'Y_pred': Y_pred,
-                 'L_pred': L_pred,
-                 'Y_true': Y_true,
-                 'L_true': L_true,
-                 'mse': -1, 'mae': -1, 'auc': auc}
-    return eval_dict
+        raise Y_pred_batch
 
-default_args = {
-    'window_size': 10,
-    'sub_dim': 1,
-    'RNN_hid_dim': 10,
-    'batch_size': 128,
-    'max_iter': 100,
-    'lr': 3e-4,
-    'weight_decay': 0.,
-    'momentum': 0.,
-    'grad_clip': 10.,
-    'CRITIC_ITERS': 5,
-    'weight_clip': .1,
-    'lambda_ae': 0.001,
-    'lambda_real': 0.1
-}
+    def predict(self, ts):
 
 
+    def fit(self, ts, epoches):
+
+        sigma_list = median_heuristic(Data.Y_hankel, beta=.5)
+        self.sigma_var = torch.FloatTensor(sigma_list).to(self.device)
+
+        tbar = trange(epoches)
+        for epoch in tbar:
+            for i, batch in enumerate(train_loader):
+                # Fit critic
+                for p in netD.parameters():
+                    p.requires_grad = True
+                self._optimizeD()
+                if np.random.choice(np.arange(self.critic_iters)) == 0:
+                    for p in netD.parameters():
+                        p.requires_grad = False  # to avoid computation
+                    self._optimizeG()
 
 
-# ========= Load Dataset and initialize model=========#
-Data = DataLoader(args, trn_ratio=args.trn_ratio, val_ratio=args.val_ratio)
-netG = NetG(args, Data)
-netD = NetD(args, Data)
-
-netG_params_count = sum([p.nelement() for p in netG.parameters()])
-netD_params_count = sum([p.nelement() for p in netD.parameters()])
-
-one = torch.cuda.FloatTensor([1])
-mone = one * -1
-
-
-# ========= Setup loss function and optimizer  =========#
-optimizerG = Optim(netG.parameters(),
-                   args.optim,
-                   lr=args.lr,
-                   grad_clip=args.grad_clip,
-                   weight_decay=args.weight_decay,
-                   momentum=args.momentum)
-
-optimizerD = Optim(netD.parameters(),
-                   args.optim,
-                   lr=args.lr,
-                   grad_clip=args.grad_clip,
-                   weight_decay=args.weight_decay,
-                   momentum=args.momentum)
-
-
-# sigma for mixture of RBF kernel in MMD
-#sigma_list = [1.0]
-#sigma_list = median_heuristic(Data.Y_subspace, beta=1.)
-sigma_list = median_heuristic(Data.Y_subspace, beta=.5)
-sigma_var = torch.FloatTensor(sigma_list).cuda()
-print('sigma_list:', sigma_var)
-
-
-# ========= Main loop for adversarial training kernel with negative samples X_f + noise =========#
-Y_val = Data.val_set['Y'].numpy()
-L_val = Data.val_set['L'].numpy()
-Y_tst = Data.tst_set['Y'].numpy()
-L_tst = Data.tst_set['L'].numpy()
-
-n_batchs = int(math.ceil(len(Data.trn_set['Y']) / float(args.batch_size)))
-print('n_batchs', n_batchs, 'batch_size', args.batch_size)
-
-lambda_ae = args.lambda_ae
-lambda_real = args.lambda_real
-gen_iterations = 0
-total_time = 0.
-best_epoch = -1
-best_val_mae = 1e+6
-best_val_auc = -1
-best_tst_auc = -1
-best_mmd_real = 1e+6
-start_time = time.time()
-print('start training: lambda_ae', lambda_ae, 'lambda_real', lambda_real, 'weight_clip', args.weight_clip)
-for epoch in range(1, args.max_iter + 1):
-    trn_loader = Data.get_batches(Data.trn_set, batch_size=args.batch_size, shuffle=True)
-    bidx = 0
-    while bidx < n_batchs:
-        ############################
-        # (1) Update D network
-        ############################
-        for p in netD.parameters():
-            p.requires_grad = True
-
-        for diters in range(args.CRITIC_ITERS):
-            # clamp parameters of NetD encoder to a cube
-            for p in netD.rnn_enc_layer.parameters():
-                p.data.clamp_(-args.weight_clip, args.weight_clip)
-            if bidx == n_batchs:
-                break
-
-            inputs = next(trn_loader)
-            X_p, X_f, Y_true = inputs[0], inputs[1], inputs[2]
-            batch_size = X_p.size(0)
-            bidx += 1
-
-            # real data
-            X_p_enc, X_p_dec = netD(X_p)
-            X_f_enc, X_f_dec = netD(X_f)
-
-            # fake data
-            noise = torch.cuda.FloatTensor(1, batch_size, args.RNN_hid_dim).normal_(0, 1)
-            noise = Variable(noise, volatile=True) # total freeze netG
-            Y_f = Variable(netG(X_p, X_f, noise).data)
-            Y_f_enc, Y_f_dec = netD(Y_f)
-
-            # batchwise MMD2 loss between X_f and Y_f
-            D_mmd2 = batch_mmd2_loss(X_f_enc, Y_f_enc, sigma_var)
-
-            # batchwise MMD loss between X_p and X_f
-            mmd2_real = batch_mmd2_loss(X_p_enc, X_f_enc, sigma_var)
-
-            # reconstruction loss
-            real_L2_loss = torch.mean((X_f - X_f_dec)**2)
-            #real_L2_loss = torch.mean((X_p - X_p_dec)**2)
-            fake_L2_loss = torch.mean((Y_f - Y_f_dec)**2)
-            #fake_L2_loss = torch.mean((Y_f - Y_f_dec)**2) * 0.0
-
-            # update netD
-            netD.zero_grad()
-            lossD = D_mmd2.mean() - lambda_ae * (real_L2_loss + fake_L2_loss) - lambda_real * mmd2_real.mean()
-            #lossD = 0.0 * D_mmd2.mean() - lambda_ae * (real_L2_loss + fake_L2_loss) - lambda_real * mmd2_real.mean()
-            #lossD = -real_L2_loss
-            lossD.backward(mone)
-            optimizerD.step()
-
-        ############################
-        # (2) Update G network
-        ############################
-        for p in netD.parameters():
-            p.requires_grad = False  # to avoid computation
-
-        if bidx == n_batchs:
-            break
-
-        inputs = next(trn_loader)
-        X_p, X_f = inputs[0], inputs[1]
+    def _optimizeG(self, batch, opt, grad_clip:int=10):
+        one = torch.FloatTensor([1])
+        X_p, X_f = batch[0], batch[1]
         batch_size = X_p.size(0)
-        bidx += 1
 
         # real data
         X_f_enc, X_f_dec = netD(X_f)
@@ -297,43 +183,45 @@ for epoch in range(1, args.max_iter + 1):
         lossG = G_mmd2.mean()
         #lossG = 0.0 * G_mmd2.mean()
         lossG.backward(one)
-        optimizerG.step()
 
-        #G_mmd2 = Variable(torch.FloatTensor(batch_size).zero_())
-        gen_iterations += 1
+        torch.nn.utils.clip_grad_norm_(self.netG.parameters(), grad_clip)
 
-        print('[%5d/%5d] [%5d/%5d] [%6d] D_mmd2 %.4e G_mmd2 %.4e mmd2_real %.4e real_L2 %.6f fake_L2 %.6f'
-              % (epoch, args.max_iter, bidx, n_batchs, gen_iterations,
-                 D_mmd2.mean().data[0], G_mmd2.mean().data[0], mmd2_real.mean().data[0],
-                 real_L2_loss.data[0], fake_L2_loss.data[0]))
+        opt.step()
 
-        if gen_iterations % args.eval_freq == 0:
-            # ========= Main block for evaluate MMD(X_p_enc, X_f_enc) on RNN codespace  =========#
-            val_dict = valid_epoch(Data, Data.val_set, netD, args.batch_size, Y_val, L_val)
-            tst_dict = valid_epoch(Data, Data.tst_set, netD, args.batch_size, Y_tst, L_tst)
-            total_time = time.time() - start_time
-            print('iter %4d tm %4.2fm val_mse %.1f val_mae %.1f val_auc %.6f'
-                    % (epoch, total_time / 60.0, val_dict['mse'], val_dict['mae'], val_dict['auc']), end='')
 
-            print (" tst_mse %.1f tst_mae %.1f tst_auc %.6f" % (tst_dict['mse'], tst_dict['mae'], tst_dict['auc']), end='')
+    def _optimizeD(self, batch, opt, grad_clip:int=10):
+        one = torch.cuda.FloatTensor([1])
+        mone = one * -1
 
-            assert(np.isnan(val_dict['auc']) != True)
-            #if val_dict['auc'] > best_val_auc:
-            #if val_dict['auc'] > best_val_auc and mmd2_real.mean().data[0] < best_mmd_real:
-            if mmd2_real.mean().data[0] < best_mmd_real:
-                best_mmd_real = mmd2_real.mean().data[0]
-                best_val_mae = val_dict['mae']
-                best_val_auc = val_dict['auc']
-                best_tst_auc = tst_dict['auc']
-                best_epoch = epoch
-                save_pred_name = '%s/pred.pkl' % (args.save_path)
-                with open(save_pred_name, 'wb') as f:
-                    pickle.dump(tst_dict, f)
-                torch.save(netG.state_dict(), '%s/netG.pkl' % (args.save_path))
-                torch.save(netD.state_dict(), '%s/netD.pkl' % (args.save_path))
-            print(" [best_val_auc %.6f best_tst_auc %.6f best_epoch %3d]" % (best_val_auc, best_tst_auc, best_epoch))
+        X_p, X_f, Y_true = batch[0], batch[1], batch[2]
+        batch_size = X_p.size(0)
 
-        # stopping condition
-        #if best_mmd_real < 1e-4:
-        if mmd2_real.mean().data[0] < 1e-5:
-            exit(0)
+        # real data
+        X_p_enc, X_p_dec = netD(X_p)
+        X_f_enc, X_f_dec = netD(X_f)
+
+        # fake data
+        noise = torch.FloatTensor(1, batch_size, args.RNN_hid_dim).normal_(0, 1).to(self.device)
+        noise = Variable(noise, volatile=True) # total freeze netG
+        Y_f = Variable(netG(X_p, X_f, noise).data)
+        Y_f_enc, Y_f_dec = netD(Y_f)
+
+        # batchwise MMD2 loss between X_f and Y_f
+        D_mmd2 = batch_mmd2_loss(X_f_enc, Y_f_enc, sigma_var)
+
+        # batchwise MMD loss between X_p and X_f
+        mmd2_real = batch_mmd2_loss(X_p_enc, X_f_enc, sigma_var)
+
+        # reconstruction loss
+        real_L2_loss = torch.mean((X_f - X_f_dec)**2)
+        fake_L2_loss = torch.mean((Y_f - Y_f_dec)**2)
+
+        # update netD
+        netD.zero_grad()
+        lossD = D_mmd2.mean() - self.lambda_ae * (real_L2_loss + fake_L2_loss) - self.lambda_real * mmd2_real.mean()
+        lossD.backward(mone)
+
+        torch.nn.utils.clip_grad_norm_(self.netD.parameters(), grad_clip)
+
+        opt.step()
+
